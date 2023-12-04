a54fc23cc0b312478048bc802e0d1b26d5d757b9
diegopaff <110573986+diegopaff@users.noreply.github.com>
2023-12-04 12:48:14 -0300

    thumbnails changed to local

--- devPortfolio/src/components/Hero.jsx
+++ devPortfolio/src/components/Hero.jsx
@@ -1,60 +1,72 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div className='
+    <div
+      className="
       bg-[#0F172A] h-full flex flex-col w-full max-w-[1600px] mx-auto gap-[8%] mt-[20%]
-      md:flex-row md:align-middle md:mt-0'>
-
-      <div className='
+      md:flex-row md:align-middle md:mt-0"
+    >
+      <div
+        className="
         flex flex-col w-full px-8 gap-7 mt-7
         md:align-middle md:justify-center md:mt-0 md:pl-[150px]
-        '>
-        <h2 className='
+        "
+      >
+        <h2
+          className="
           font-pacifico text-4xl text-white text-center
           md:text-left md:text-5xl
-          lg:text-7xl'>
-            Hi,
+          lg:text-7xl"
+        >
+          Hi,
         </h2>
-        <h2 className='font-pacifico text-4xl text-white text-center
+        <h2
+          className="font-pacifico text-4xl text-white text-center
           md:text-left md:text-5xl
-          lg:text-7xl'>
-            I am <span className=' text-[#7477FF] '>D</span>iego,
+          lg:text-7xl"
+        >
+          I am <span className=" text-[#7477FF] ">D</span>iego,
         </h2>
 
-        <h2 className='font-pacifico text-4xl text-white text-center
+        <h2
+          className="font-pacifico text-4xl text-white text-center
           md:text-left md:text-5xl
-          lg:text-7xl'> 
+          lg:text-7xl"
+        >
           Web Developer
         </h2>
-        <p className='font-poppins font-light text-sm text-white text-center
-                      lg:text-left'>
+        <p
+          className="font-poppins font-light text-sm text-white text-center
+                      lg:text-left"
+        >
           Frontend Developer, Profficienct in React & Node
         </p>
-        <a 
-          href='#Projects' 
-          className='mx-auto w-[40%] flex justify-center
-              lg:mx-0 lg:justify-start'>
-          <button className={`bg-[#7477FF] hover:bg-[#6a6cff] text-white font-bold py-2 px-4 rounded items-center`}>
+        <a
+          href="#Projects"
+          className="mx-auto w-[40%] flex justify-center
+              lg:mx-0 lg:justify-start"
+        >
+          <button
+            className={`bg-[#7477FF] hover:bg-[#6a6cff] text-white font-bold py-2 px-4 rounded items-center`}
+          >
             <span>See projects</span>
           </button>
         </a>
       </div>
-      <div className='
+      <div
+        className="
         flex w-full px-10
-        sm:align-middle sm:justify-center'>
-        <img 
-          className='w-full object-contain
-                     sm:max-w-md'
-          src='https://i.ibb.co/jvqcTZx/hero-img.png'></img>
+        sm:align-middle sm:justify-center"
+      >
+        <img
+          className="w-full object-contain
+                     sm:max-w-md"
+          src="../assets/hero-img-min.png"
+        ></img>
       </div>
-      
-
-      
-
-      
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- devPortfolio/src/constants/index.js
+++ devPortfolio/src/constants/index.js
@@ -1,70 +1,70 @@
 export const navLinks = [
-    {
-        id: 'about',
-        title: 'About',
-    },
-    {
-        id: 'skills',
-        title: 'Skills',
-    },
-    {
-        id: 'projects',
-        title: 'Projects',
-    },
-    {
-        id: 'contact',
-        title: 'Contact',
-    },
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "skills",
+    title: "Skills",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
 ];
 
-
 export const projectsList = [
-    {
-        title: 'E-commerce',
-        tools: ['React Js','Firebase', 'Javascript', 'CSS'],
-        thumbnail: 'https://i.ibb.co/qdhXD13/Eshop.jpg',
-        description: 'This project its a e-commerce that fetch data from a firestore database and let you see and shop in a simple way.',
-        liveLink: 'https://eshopcommerce.netlify.app/',
-        repoLink: 'https://github.com/diegopaff/coder_react'
-    },
-    {
-        title: 'Video Editor Portfolio',
-        tools: ['React Js','Tailwind', 'Javascript', 'Email-js'],
-        thumbnail: 'https://i.ibb.co/GT0fL9N/Editor-Portfolio.jpg',
-        description: "This project it's a static page that showcase all the latest work of a video editor, you can contact him via form and see and download his Resume." ,
-        liveLink: 'https://diegocurutcheteditor.netlify.app/',
-        repoLink: 'https://github.com/diegopaff/portfolio_av'
-    },
-    {
-        title: 'Movie watchlist',
-        tools: ['Javascript', 'CSS', 'HTML5'],
-        thumbnail: 'https://i.ibb.co/Rjcf3SR/movie-Watch.jpg',
-        description: "The objective of this website is to satisfy my passion for cinema and complete checklists. This way I can add all the movies I already watched and the ones I want to watch in a simple way." ,
-        liveLink: 'https://diegopaff.github.io/movieWatch/',
-        repoLink: 'https://github.com/diegopaff/portfolio_av'
-    },
-
-
-]
+  {
+    title: "E-commerce",
+    tools: ["React Js", "Firebase", "Javascript", "CSS"],
+    thumbnail: "../assets/Eshop-min.jpg",
+    description:
+      "This project its a e-commerce that fetch data from a firestore database and let you see and shop in a simple way.",
+    liveLink: "https://eshopcommerce.netlify.app/",
+    repoLink: "https://github.com/diegopaff/coder_react",
+  },
+  {
+    title: "Video Editor Portfolio",
+    tools: ["React Js", "Tailwind", "Javascript", "Email-js"],
+    thumbnail: "../assets/EditorPortfolio-min.jpg",
+    description:
+      "This project it's a static page that showcase all the latest work of a video editor, you can contact him via form and see and download his Resume.",
+    liveLink: "https://diegocurutcheteditor.netlify.app/",
+    repoLink: "https://github.com/diegopaff/portfolio_av",
+  },
+  {
+    title: "Movie watchlist",
+    tools: ["Javascript", "CSS", "HTML5"],
+    thumbnail: "../assets/movieWatch-min.jpg",
+    description:
+      "The objective of this website is to satisfy my passion for cinema and complete checklists. This way I can add all the movies I already watched and the ones I want to watch in a simple way.",
+    liveLink: "https://diegopaff.github.io/movieWatch/",
+    repoLink: "https://github.com/diegopaff/portfolio_av",
+  },
+];
 
 export const smallProjectsList = [
-    {
-        title: 'Remote dev jobs search',
-        tools: ['Javascript','HTML', 'CSS', 'Webpack', 'API fetch'],
-        thumbnail: '',
-        description: 'This project its Single page aplication that shows a list jobs (fake) consumed of an API, and',
-        liveLink: '',
-        repoLink: ''
-    },
-    {
-        title: 'Responsive marketing agency landing page',
-        tools: ['React','Javascript','HTML', 'SASS', 'CSS'],
-        thumbnail: 'https://i.ibb.co/0cb7tzc/Front-End-Mendtor-CH4-hero-desktop.jpg',
-        description: "This project it's part of a challenge from frontendmentor.com, they give you the images and the design and you deliver pixel perfect production of the website.",
-        liveLink: 'https://front-m-challenge04.netlify.app/',
-        repoLink: 'https://github.com/diegopaff/Frontend-Mentor_ch_04'
-    },
-
-] 
-
- 
+  {
+    title: "Remote dev jobs search",
+    tools: ["Javascript", "HTML", "CSS", "Webpack", "API fetch"],
+    thumbnail: "",
+    description:
+      "This project its Single page aplication that shows a list jobs (fake) consumed of an API, and",
+    liveLink: "",
+    repoLink: "",
+  },
+  {
+    title: "Responsive marketing agency landing page",
+    tools: ["React", "Javascript", "HTML", "SASS", "CSS"],
+    thumbnail:
+      "https://i.ibb.co/0cb7tzc/Front-End-Mendtor-CH4-hero-desktop.jpg",
+    description:
+      "This project it's part of a challenge from frontendmentor.com, they give you the images and the design and you deliver pixel perfect production of the website.",
+    liveLink: "https://front-m-challenge04.netlify.app/",
+    repoLink: "https://github.com/diegopaff/Frontend-Mentor_ch_04",
+  },
+];
